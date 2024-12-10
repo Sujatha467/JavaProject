ab56daf963ab91c22cd2f04eece9ce4c4aefa902
Sujatha467 <sujathacibi@gmail.com>
2024-12-10 18:36:52 +0530

    Filed Added

--- BankingSystem/Main.java
+++ BankingSystem/Main.java
@@ -1,6 +1,7 @@
+
 import java.util.Scanner;
 
-public class Maingut {
+public class Main {
     public static void main(String[] args) {
         BankManager manager = new BankManager();
         Scanner scanner = new Scanner(System.in);
